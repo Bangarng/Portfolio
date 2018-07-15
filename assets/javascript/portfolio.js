73430b90e5b391d4961dd8d746d5d60df2ba19de
Dickie Stevens <rsk0528@gmail.com>
2018-07-15 19:55:46 -0400

    background changes

---
+++ assets/javascript/portfolio.js
@@ -0,0 +1,6 @@
+/*******************************PARTICLE JS ********************************/
+/* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
+particlesJS.load('particles-js', 'assets/javascript/particles.json', function() {
+console.log('callback - particles.js config loaded');
+});
+
